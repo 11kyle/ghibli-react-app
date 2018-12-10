6af12bc6a4e87c9855a299deabd77f32e28fbfe4
Kyle <kyle@Kyles-MacBook-Pro.local>
2018-12-09 21:25:17 -0700

    Updates

---
+++ precache-manifest.51f52209e7f7d4b4ef2ce408c2b2bdcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e86d97c08805ce75a6e83bbd1c272455",
+    "url": "/ghibli-react-app/static/media/logo.e86d97c0.png"
+  },
+  {
+    "revision": "abd64e2e4b18ec7fa061",
+    "url": "/ghibli-react-app/static/js/runtime~main.abd64e2e.js"
+  },
+  {
+    "revision": "8ac392264be12e407da1",
+    "url": "/ghibli-react-app/static/js/main.8ac39226.chunk.js"
+  },
+  {
+    "revision": "f816b3cce3f9a7d9943b",
+    "url": "/ghibli-react-app/static/js/1.f816b3cc.chunk.js"
+  },
+  {
+    "revision": "8ac392264be12e407da1",
+    "url": "/ghibli-react-app/static/css/main.63f23acd.chunk.css"
+  },
+  {
+    "revision": "a6bca98be17e746ec5d047fada749344",
+    "url": "/ghibli-react-app/index.html"
+  }
+];
